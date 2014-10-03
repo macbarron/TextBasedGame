d78240db5cc6b58477190b7a4c90189633dc1707
JeffBaucom <jeffbaucom@gmail.com>
2014-10-03 15:35:27 -0400

    Update Player Class

--- Player.java
+++ Player.java
@@ -1,1 +1,31 @@
+public class Player {
+
+	private static int currentLocation;
+	private static int health = 100;
 	
+	public static void main(String[] args) {
+		while (true) {
+	    try {
+	        Thread.sleep(300 * 1000); // the argument here represents milliseconds, currently configured to 5 minutes
+	    }
+	    catch (InterruptedException ie) {
+	        ie.printStackTrace();
+	    }
+	    changeHealth();
+
+		}
+	}	
+
+	public static void changeHealth(){
+		health = health - 5;
+		System.out.println(health);
+	}
+
+	public static int getHealth(){
+		return health;
+	}
+
+	public static void changeHealth(int change){
+		health += change;
+	}
+}
