98ab235b7fa9c517f1cef63fe179954f10d0ba6b
JeffBaucom <jeffbaucom@gmail.com>
2014-10-03 15:59:34 -0400

    Location Class Skeleton
    Added Constructor with World ArrayList to contain all Location objects

--- Locations/Location.java
+++ Locations/Location.java
@@ -1,3 +1,20 @@
+import java.util.ArrayList;
+
 public class Location {
-	
+
+	public static ArrayList<Location> world = new ArrayList<Location>();
+	public static int idCounter = 0;
+	public int id;
+	public String title;
+
+
+	/**
+	*Super Constructor for all Locations
+	*/
+	public Location(String title) {
+		this.title = title;
+		this.id = idCounter;
+		idCounter++;
+		world.add(this);
+	}
 }
